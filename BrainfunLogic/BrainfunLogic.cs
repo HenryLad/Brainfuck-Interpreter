49f711c74d3e3fcca09a93c707a3247e698fc219
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2024-10-08 20:34:49 +0200

    Finished Hanling without considering any loops

--- BrainfunLogic/BrainfunLogic.cs
+++ BrainfunLogic/BrainfunLogic.cs
@@ -2,7 +2,7 @@
 
 public class BrainFunLogic
 {
-   public string[] input; 
+   public string[] input;
 
    public bool ValidateInput(string path)
    {
@@ -11,31 +11,63 @@ public class BrainFunLogic
       {
          input = File.ReadAllLines(path);
       }
-      catch 
+      catch
       {
          throw new Exception($"File Path : {path} is invalid. Please check your Filepath");
       }
 
-      if(input.Length == 0){throw new Exception("The giving input in the File is not valid.");}
+      if (input.Length == 0) { throw new Exception("The giving input in the File is not valid."); }
 
       return true;
-      
-   }
-   
-   public string[] HandleInput(string UserInput = "")
-   {
-      int UserInputCounter = 0;
-      string[] output = [];
 
-      for(int i = 0; i < input.Length; i++)
+   }
+
+   public int[] HandleInput(string UserInput = "")
+   {
+      int UserInputIndex = 0;
+      int ptr = 0;
+
+      System.Console.WriteLine(UserInput);
+      int[] IntOutput = [];
+      char[] Output = [];
+
+      for (int i = 0; i < input.Length; i++)
       {
-         for(int y = 0; y < input[i].Length; y++)
+         for (int y = 0; y < input[i].Length; y++)
          {
-            
+            switch (input[i][y])
+            {
+               case '+':
+                  IntOutput[ptr]++;
+                  break;
+               case '-':
+                  IntOutput[ptr]--;
+                  break;
+               case '<':
+                  ptr--;
+                  break;
+               case '>':
+                  ptr++;
+                  break;
+               case '[':
+
+                  break;
+               case ']':
+
+                  break;
+               case ',':
+                  char inputChar = UserInput[UserInputIndex];
+                  UserInputIndex++;
+                  IntOutput[ptr] = (int)inputChar;
+                  break;
+               case '.':
+                  Output[ptr] = (char)IntOutput[ptr];
+               break;
+            }
          }
       }
 
-      return output;
+      return IntOutput;
 
    }
 }
