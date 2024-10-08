3ad278827ac6247902b8bf04786eb83596504067
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2024-10-08 20:15:02 +0200

    Setup starting interpritation Logic

--- BrainfunApp/Program.cs
+++ BrainfunApp/Program.cs
@@ -2,8 +2,6 @@
 
 
 
-using BrainfunLogic;
-
 
 
 if(args.Length == 0) {throw new Exception("Please provide the path of the input file");}
@@ -11,4 +9,13 @@ else
 {
    var bfl = new BrainFunLogic();
    if(bfl.ValidateInput(args[0])){System.Console.WriteLine("File is ready for compiling");}
+   if(args.Length == 2)
+   {
+      bfl.HandleInput(args[1]);
+   }
+   else{
+      bfl.HandleInput();
+   }
+
+
 }
---
+++ BrainfunLogic/BrainfunLogic.cs
@@ -0,0 +1,41 @@
+﻿namespace BrainfunLogic;
+
+public class BrainFunLogic
+{
+   public string[] input; 
+
+   public bool ValidateInput(string path)
+   {
+
+      try
+      {
+         input = File.ReadAllLines(path);
+      }
+      catch 
+      {
+         throw new Exception($"File Path : {path} is invalid. Please check your Filepath");
+      }
+
+      if(input.Length == 0){throw new Exception("The giving input in the File is not valid.");}
+
+      return true;
+      
+   }
+   
+   public string[] HandleInput(string UserInput = "")
+   {
+      int UserInputCounter = 0;
+      string[] output = [];
+
+      for(int i = 0; i < input.Length; i++)
+      {
+         for(int y = 0; y < input[i].Length; y++)
+         {
+            
+         }
+      }
+
+      return output;
+
+   }
+}
